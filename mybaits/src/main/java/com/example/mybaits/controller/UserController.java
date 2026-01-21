f8b3caae5c1aa30dd88657319587970737a56c5e
chenyang <chenyangliuhua@gmail.com>
2026-01-21 14:10:06 +0800

    feat: 切换为构造器方式注入依赖。

--- mybaits/src/main/java/com/example/mybaits/controller/UserController.java
+++ mybaits/src/main/java/com/example/mybaits/controller/UserController.java
@@ -2,24 +2,22 @@ package com.example.mybaits.controller;
 
 import com.example.mybaits.entity.Users;
 import com.example.mybaits.service.UsersService;
-import jakarta.annotation.Resource;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 
-    @Resource
-    private UsersService usersService;
+    private final UsersService usersService;
 
     @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
     public ResponseEntity<Users> getUser(@PathVariable Long id) {
         return ResponseEntity.of(usersService.getOptById(id));
     }
--- sse/build.gradle.kts
+++ sse/build.gradle.kts
@@ -1,9 +1,10 @@
 extra["springAiVersion"] = "1.1.2"
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.projectlombok:lombok:1.18.42")
     implementation("org.springframework.ai:spring-ai-starter-model-deepseek")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 dependencyManagement {
